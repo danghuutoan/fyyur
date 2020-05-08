55eb7beedb3ae1f64b39819d8577b2246a5df7ec
danghuutoan <danghuutoan@gmail.com>
2020-05-08 21:05:47 +0700

    updating models and populating seed data

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
+from flask_migrate import Migrate
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
@@ -20,7 +21,7 @@ app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
-
+migrate = Migrate(app, db)
 # TODO: connect to a local postgresql database
 
 #----------------------------------------------------------------------------#
@@ -38,7 +39,10 @@ class Venue(db.Model):
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-
+    website = db.Column(db.String(500), nullable=True)
+    seeking_talent = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String(500), nullable = True)
+    genres = db.Column(db.ARRAY(db.String))
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 class Artist(db.Model):
@@ -52,6 +56,9 @@ class Artist(db.Model):
     genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String(500), nullable = True)
+    website = db.Column(db.String(500), nullable = True)
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
